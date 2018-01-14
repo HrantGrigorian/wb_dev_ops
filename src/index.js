46d777b7db7a98a3568f2f7a0054e637aecb4114
Dmitri Eryomin <dmitri.eryomin@gmail.com>
2018-01-14 17:10:41 +0400

    Some refactoring

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import './styles.css';
 import printFoo from './print.js';
-// import myImage from './icon.png';
 
 function createElement() {
-    var element = document.querySelector('.hello');
+    var rootElement = document.querySelector('.root');
     var button = document.createElement("button");
 
     button.innerHTML = "Click on me";
     button.onclick = printFoo;
-    // var myIcon = new Image();
-    // myIcon.src = myImage;
 
     element.innerHTML = "Hello Webpack!";
 
     element.appendChild(button);
-    // element.appendChild(myIcon);
-
     return element;
 }
 
